d386b1828979c97a133d1662a25998fc9b3702e5
snowdream <sn0wdr1am@qq.com>
2024-07-12 14:06:14 +0800

    feat: go=1.22.5-r0

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ LABEL maintainer="snowdream <sn0wdr1am@qq.com>"
 ENV GO111MODULE=on \
     GOPROXY=https://proxy.golang.org,https://goproxy.io,direct
     
-RUN apk add --no-cache go
+RUN apk add --no-cache go=1.22.5-r0
 
 COPY docker-entrypoint.sh /usr/local/bin/
 
--- build.sh
+++ build.sh
@@ -7,19 +7,19 @@ GITHUB_PROJECT1=ghcr.io/snowdreamtech/golang
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
 -t ${DOCKER_HUB_PROJECT}:entrypoint-latest \
--t ${DOCKER_HUB_PROJECT}:entrypoint-1.22.4 \
+-t ${DOCKER_HUB_PROJECT}:entrypoint-1.22.5 \
 -t ${DOCKER_HUB_PROJECT}:entrypoint-1.22 \
 -t ${DOCKER_HUB_PROJECT}:entrypoint-1 \
 -t ${GITHUB_PROJECT}:entrypoint-latest \
--t ${GITHUB_PROJECT}:entrypoint-1.22.4 \
+-t ${GITHUB_PROJECT}:entrypoint-1.22.5 \
 -t ${GITHUB_PROJECT}:entrypoint-1.22 \
 -t ${GITHUB_PROJECT}:entrypoint-1 \
 -t ${DOCKER_HUB_PROJECT1}:entrypoint-latest \
--t ${DOCKER_HUB_PROJECT1}:entrypoint-1.22.4 \
+-t ${DOCKER_HUB_PROJECT1}:entrypoint-1.22.5 \
 -t ${DOCKER_HUB_PROJECT1}:entrypoint-1.22 \
 -t ${DOCKER_HUB_PROJECT1}:entrypoint-1 \
 -t ${GITHUB_PROJECT1}:entrypoint-latest \
--t ${GITHUB_PROJECT1}:entrypoint-1.22.4 \
+-t ${GITHUB_PROJECT1}:entrypoint-1.22.5 \
 -t ${GITHUB_PROJECT1}:entrypoint-1.22 \
 -t ${GITHUB_PROJECT1}:entrypoint-1 \
 . \
