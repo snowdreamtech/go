f35a4b0170df6f1ba6fa5f28b267061f7c09eb5f
snowdream <sn0wdr1am@qq.com>
2024-08-17 15:18:13 +0800

    feat： go 1.22.6

--- build.sh
+++ build.sh
@@ -7,19 +7,19 @@ GITHUB_PROJECT1=ghcr.io/snowdreamtech/golang
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
     -t ${DOCKER_HUB_PROJECT}:latest \
-    -t ${DOCKER_HUB_PROJECT}:1.22.5 \
+    -t ${DOCKER_HUB_PROJECT}:1.22.6 \
     -t ${DOCKER_HUB_PROJECT}:1.22 \
     -t ${DOCKER_HUB_PROJECT}:1 \
     -t ${GITHUB_PROJECT}:latest \
-    -t ${GITHUB_PROJECT}:1.22.5 \
+    -t ${GITHUB_PROJECT}:1.22.6 \
     -t ${GITHUB_PROJECT}:1.22 \
     -t ${GITHUB_PROJECT}:1 \
     -t ${DOCKER_HUB_PROJECT1}:latest \
-    -t ${DOCKER_HUB_PROJECT1}:1.22.5 \
+    -t ${DOCKER_HUB_PROJECT1}:1.22.6 \
     -t ${DOCKER_HUB_PROJECT1}:1.22 \
     -t ${DOCKER_HUB_PROJECT1}:1 \
     -t ${GITHUB_PROJECT1}:latest \
-    -t ${GITHUB_PROJECT1}:1.22.5 \
+    -t ${GITHUB_PROJECT1}:1.22.6 \
     -t ${GITHUB_PROJECT1}:1.22 \
     -t ${GITHUB_PROJECT1}:1 \
     . \
