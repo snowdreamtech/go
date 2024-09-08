f39cb8ef58447d8c1c70d322285ebb03ebef4ce2
snowdream <sn0wdr1am@qq.com>
2024-09-08 11:17:58 +0800

    feat: ENV KEEPALIVE (support ctrl+c)

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM snowdreamtech/alpine:3.20.2
 
 LABEL maintainer="snowdream <sn0wdr1am@qq.com>"
 
-ENV GO111MODULE=on \
+# keep the docker container running
+ENV KEEPALIVE=0 \
+    GO111MODULE=on \
     GOPROXY=https://proxy.golang.org,https://goproxy.io,direct
     
 RUN apk add --no-cache go=1.22.7-r0
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,14 @@
 set -e
 
 # exec commands
-exec "$@"
+if [ -n "$*" ]; then
+    sh -c "$*"
+fi
+
+# keep the docker container running
+# https://github.com/docker/compose/issues/1926#issuecomment-422351028
+if [ "${KEEPALIVE}" -eq 1 ]; then
+    trap : TERM INT
+    tail -f /dev/null & wait
+    # sleep infinity & wait
+fi
